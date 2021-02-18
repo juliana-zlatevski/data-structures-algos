c67562d0b54019017cf7babcb4b34220f48556f4
juliana-zlatevski <juliana.zlatevski@hotmail.com>
2021-02-18 16:40:31 -0600

    adds linked list examples and notes

--- scripts/codecademyNotes.js
+++ scripts/codecademyNotes.js
@@ -115,3 +115,107 @@ while (currentNode != null) {
 }
 
 module.exports = Node;
+
+// LINKED LISTS
+// linking nodes together using their next property creates a singly linked list
+// tail node is the end of a linked list, head node is the first node.
+
+// common operations:
+// adding, removing, finding, traversing (or traveling through) the linked list
+
+const Node = require('./Node');
+
+class LinkedList {
+  
+  constructor() {
+    this.head = null;
+  }
+
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+    this.head = newHead;
+    if (currentHead) {
+      this.head.setNextNode(currentHead);
+    }
+  }
+
+  addToTail(data) {
+    let tail = this.head;
+    if (!tail) {
+      this.head = new Node(data);
+    } else {
+      while(tail.getNextNode() !== null) {
+        tail = tail.getNextNode();
+      }
+      tail.setNextNode(new Node(data));
+    }
+  }
+}
+
+module.exports = LinkedList;
+
+
+const Node = require('./Node');
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+    this.head = newHead;
+    if (currentHead) {
+      this.head.setNextNode(currentHead);
+    }
+  }
+
+  addToTail(data) {
+    let tail = this.head;
+    if (!tail) {
+      this.head = new Node(data);
+    } else {
+      while (tail.getNextNode() !== null) {
+        tail = tail.getNextNode();
+      }
+      tail.setNextNode(new Node(data));
+    }
+  }
+
+  removeHead() {
+    const removedHead = this.head;
+    if (!removedHead) {
+      return;
+    }
+    this.head = removedHead.getNextNode();
+    return removedHead.data;
+  }
+
+  printList() {
+    let currentNode = this.head;
+    let output = '<head> ';
+    while (currentNode !== null) {
+      output += currentNode.data + ' ';
+      currentNode = currentNode.getNextNode();
+    }
+    output += '<tail>';
+    console.log(output);
+  }
+
+}
+
+module.exports = LinkedList;
+
+const LinkedList = require('./LinkedList');
+
+const seasons = new LinkedList();
+
+seasons.addToHead('summer');
+seasons.addToHead('spring');
+seasons.addToTail('fall');
+seasons.addToTail('winter');
+seasons.removeHead();
+
+seasons.printList();
