cedf8a6b9fddf7f124458b2686523973fb9be2e6
juliana-zlatevski <juliana.zlatevski@hotmail.com>
2021-02-19 13:43:26 -0600

    doubly linked list example and notes

--- scripts/codecademyNotes.js
+++ scripts/codecademyNotes.js
@@ -219,3 +219,132 @@ seasons.addToTail('winter');
 seasons.removeHead();
 
 seasons.printList();
+
+// DOUBLY LINKED LISTS
+// contain links to the nodes prior to current node as well
+// common operations include adding to both ends, removing from both ends, finding and removing from anywhere in the list, traversing through the list
+
+const Node = require('./Node');
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+    if (currentHead) {
+      currentHead.setPreviousNode(newHead);
+      newHead.setNextNode(currentHead);
+    }
+    this.head = newHead;
+    if (!this.tail) {
+      this.tail = newHead;
+    }
+  }
+  
+  addToTail(data) {
+    const newTail = new Node(data);
+    const currentTail = this.tail;
+    if (currentTail) {
+      currentTail.setNextNode(newTail);
+      newTail.setPreviousNode(currentTail);
+    }
+    this.tail = newTail;
+    if (!this.head) {
+      this.head = newTail;
+    }
+  }
+  
+  removeHead() {
+    const removedHead = this.head;
+    if (!removedHead) {
+      return;
+    }
+    this.head = removedHead.getNextNode();
+    if (this.head) {
+      this.head.setPreviousNode(null);
+    }
+    if (removedHead === this.tail) {
+      this.removeTail();
+    }
+    return removedHead.data;
+  }
+  
+  removeTail() {
+    const removedTail = this.tail;
+    if (!removedTail) {
+      return;
+    }
+    this.tail = removedTail.getPreviousNode();
+    if (this.tail) {
+      this.tail.setNextNode(null);
+    }
+    if (removedTail === this.head) {
+      this.removeHead();
+    }
+    return removedTail.data;
+  }
+  
+  removeByData(data) {
+    let nodeToRemove;
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.data === data) {
+        nodeToRemove = currentNode;
+        break;
+      }
+      currentNode = currentNode.getNextNode();
+    }
+    if (!nodeToRemove) {
+      return null;
+    }
+    if (nodeToRemove === this.head) {
+      this.removeHead();
+    } else if(nodeToRemove === this.tail) {
+      this.removeTail();
+    } else {
+      const nextNode = nodeToRemove.getNextNode();
+      const previousNode = nodeToRemove.getPreviousNode();
+      nextNode.setPreviousNode(previousNode);
+      previousNode.setNextNode(nextNode);
+    }
+    return nodeToRemove;
+  }
+  
+  printList() {
+    let currentNode = this.head;
+    let output = '<head> ';
+    while (currentNode !== null) {
+      output += currentNode.data + ' ';
+      currentNode = currentNode.getNextNode();
+    }
+    output += '<tail>';
+    console.log(output);
+  }
+}
+
+module.exports = DoublyLinkedList;
+
+// using the doubly linked list:
+const DoublyLinkedList = require('./DoublyLinkedList.js');
+
+const subway = new DoublyLinkedList();
+
+subway.addToHead('TimesSquare');
+subway.addToHead('GrandCentral');
+subway.addToHead('CentralPark');
+
+subway.addToTail('PennStation');
+subway.addToTail('WallStreet');
+subway.addToTail('BrooklynBridge');
+
+subway.removeHead();
+subway.removeTail();
+
+subway.removeByData('TimesSquare');
+
+subway.printList();
+
